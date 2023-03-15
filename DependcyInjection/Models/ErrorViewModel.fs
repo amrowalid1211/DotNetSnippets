adeda371902a36f88d22aeab0cbbbe6fbd0a5b07
siriusamro <73645885+siriusamro@users.noreply.github.com>
2023-03-16 01:58:29 +0200

    Add project files.

---
+++ DependcyInjection/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace DependcyInjection.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
